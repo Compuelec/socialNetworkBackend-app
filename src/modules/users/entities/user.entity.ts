0ffb19727fed4d7a596a25cb8c75ac4096c9bc91
Hernán <he.vallejos@duocuc.cl>
2024-03-19 19:44:20 -0300

    update: create user entity

--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -1,1 +1,38 @@
-export class User {}
+import {  Column, DeleteDateColumn, Entity } from 'typeorm';
+
+@Entity('users')
+export class User {
+    @Column({ primary: true, generated: true })
+    id: number;
+
+    @Column()
+    firstName: string;
+
+    @Column()
+    lastName: string;
+
+    @Column()
+    age: number;
+
+    @Column()
+    email: string;
+
+    @Column( {nullable: false})
+    password: string;
+
+    @Column()
+    phoneNumber: string;
+
+    @Column()
+    address: string;
+
+    @Column()
+    role: string;
+
+    @Column()
+    isActive: boolean;
+
+    @DeleteDateColumn() //deja registro de cuando se elimina un registro
+    deletedAt: Date;
+
+}
